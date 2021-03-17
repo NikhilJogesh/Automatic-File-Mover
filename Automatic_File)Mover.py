10a27fbcddedef563cd0f5dd3a2d0c72774e55c5
Nikhil Jogesh <nikhiljogesh2007@gmail.com>
2021-03-17 17:38:28 +0530

    Added json support, now users can modify their path values through a json file. Finally added comment(LOL)

--- Automatic_File)Mover.py
+++ Automatic_File)Mover.py
@@ -1,34 +1,53 @@
+# imports all dependent modules
 import os
 import shutil
+import json
 
-main_path = 'D:/Nikhil/Python Projects/Automatic File Mover/Main/'
+# opens the json file containing the paths 
+with open('paths.json', ) as f:
+	# loads the file
+	paths = json.load(f)
+	# access the dictionary present in the list
+	paths = paths[0]
 
-images_folder_path = 'D:/Nikhil/Python Projects/Automatic File Mover/Images'
-doccument_folder_path = 'D:/Nikhil/Python Projects/Automatic File Mover/Documents'
-music_folder_path = 'D:/Nikhil/Python Projects/Automatic File Mover/Music'
-videos_folder_path = 'D:/Nikhil/Python Projects/Automatic File Mover/Videos'
-zips_and_installers_folder_path = 'D:/Nikhil/Python Projects/Automatic File Mover/Zips and Installers'
+# add a "\\" to each path to avoid errors
+for key in paths.keys():
+	path = paths[key]
+	if type(path) == str:
+		path = f'{path}\\'
+		paths[key] = path
+	
+# sets the path for the main folder
+main_path = paths['main_folder']
 
+#sets the folder path for every file group\type
+images_folder_path = paths['images_path']
+doccument_folder_path = paths['documents_path']
+music_folder_path = paths['songs_path']
+videos_folder_path = ['videos_path']
+zips_and_installers_folder_path = ['zips_executables_path']
 
+# creating a list of file formats for each file groups\type 
 images_extensions = ['.jpg', '.png', '.gif', '.bmp']
 document_extensions = ['.docx', '.txt', '.pdf']
 music_extensions = ['.mp3', '.wav']
 videos_extensions = ['.mpeg', '.mpg', '.mp4', '.mkv', '.mov', '.avi']
 zips_and_installers_extensions = ['.exe', '.gz', '.rar', '.zip']
 
-
+# sets the main folder
 main_dir = os.listdir(main_path)
  
-
-
+# for every file in the main folder, 
+# it checks its file type
+# and moves it to its respective folder
 for file in main_dir:
-	
+	# splits the file name and sets the file extension to a variable
 	splited_name = (os.path.splitext(file))
 	extention = splited_name[1]
-	print(extention)
-
+	# sets the full path location of the file
 	file_path = main_path + file
 	
+	# checks the file extension and moves to its destination folder
 	if images_extensions.count(extention) > 0: 
 		shutil.move(file_path , images_folder_path)
 
